a1603ed2232add836b1496ae14923c9929fadf25
Slayter <slayter007@gmail.com>
2021-03-30 00:38:09 +0300

    Update component

---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <podcast-sample></podcast-sample>
+    <podcast-sample></podcast-sample>
+    <podcast-sample></podcast-sample>
+  </div>
+</template>
+
+<script>
+import PodcastSample from './podcast/Sample'
+export default {
+  name: 'home',
+  components: {
+    PodcastSample
+  }
+}
+</script>
---
+++ src/components/TopHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    <player></player>
+  </header>
+</template>
+
+<script>
+import Player from './player/Player'
+export default {
+  name: 'top-header',
+  components: {
+    Player
+  }
+}
+</script>
